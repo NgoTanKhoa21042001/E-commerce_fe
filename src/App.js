1cf3d9d97534f3ef9b45f62842b747c345cc223f
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2022-12-18 12:26:33 +0700

    Create Login and Signup pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,8 @@ function App() {
         <Navigation />
         <Routes>
           <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="*" element={<Home />} />
         </Routes>
       </BrowserRouter>
