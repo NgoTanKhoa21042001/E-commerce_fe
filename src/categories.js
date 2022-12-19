4b9bb4865e777e185a5544402d9ead2291a4ccc8
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2022-12-19 21:40:58 +0700

    complete Home page

---
+++ src/categories.js
@@ -0,0 +1,20 @@
+const categories = [
+  {
+    name: "Technology",
+    img: "https://res.cloudinary.com/bach22/image/upload/v1671457593/avatar/photo-1516035069371-29a1b244cc32_dtwymv.jpg",
+  },
+  {
+    name: "Phone",
+    img: "https://res.cloudinary.com/bach22/image/upload/v1671457673/avatar/photo-1601784551446-20c9e07cdbdb_vlu79x.jpg",
+  },
+  {
+    name: "Technology",
+    img: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+];
+
+export default categories;
+
+// https://res.cloudinary.com/bach22/image/upload/v1671457673/avatar/photo-1601784551446-20c9e07cdbdb_vlu79x.jpg
+
+// https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,66 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
-
+import { Col, Row } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
+import categories from "../categories";
+import "./Home.css";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <img
+        src="https://res.cloudinary.com/learn-code-10/image/upload/v1653947013/yqajnhqf7usk56zkwqi5.png"
+        alt=""
+        className="home-banner"
+      />
+      <div className="featured-products-container container mt-4">
+        <h2>Last products</h2>
+        {/* last products here */}
+        <div>
+          <Link
+            to="/category/all"
+            style={{
+              textAlign: "right",
+              display: "block",
+              textDecoration: "none",
+            }}
+          >
+            See more {">>"}
+          </Link>
+        </div>
+      </div>
+      {/* sale banner */}
+      <div className="sale_banner--container mt-4">
+        <img
+          src="https://thumbs.dreamstime.com/b/online-shopping-banner-bags-icons-shop-61959143.jpg"
+          alt=""
+        />
+      </div>
+      <div className="recent-products-container container mt-4">
+        <h2>Categories</h2>
+        <Row>
+          {categories.map((category, idx) => (
+            <LinkContainer
+              key={idx}
+              to={`/category/${category.name.toLocaleLowerCase()}`}
+            >
+              <Col md={4}>
+                <div
+                  style={{
+                    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${category.img})`,
+                    gap: "10px",
+                  }}
+                  className="category-tile"
+                >
+                  {category.name}
+                </div>
+              </Col>
+            </LinkContainer>
+          ))}
+        </Row>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
